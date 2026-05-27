97dcd77bc23095fab826912d56e1c6db7a7bc930
Harmanjot Singh <157384176+Harman0640@users.noreply.github.com>
2026-05-27 15:17:27 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,5 @@
 let mongoose = require('mongoose')
-const dbPath = 'mongodb+srv://jaspreet:1Jaspreet@student.civhn6j.mongodb.net/bicycleDb'
+const dbPath = 'mongodb+srv://harmanjot1251_db_user:harman5602@cluster0.bht3ipe.mongodb.net/bicycleDb'
 const options = { useNewUrlParser: true, useUnifiedTopology: true }
 mongoose.connect(dbPath, options).then(res => {
     console.log("Db Connected")
